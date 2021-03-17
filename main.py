600b893fe4184e8e7e57b92bb3b344caffbe1533
Dylan Stockard <dylstockard@live.com>
2021-03-16 20:20:46 -0700

    Nlp class
    Edited nlp.py to be a class that can be called from main. Can call nlp.predict() on an excel file of reviews to get a new excel sheet with sentiments in a new column.

--- main.py
+++ main.py
@@ -3,20 +3,37 @@ import os
 import tarfile
 import pandas as pd
 import json
+from nlp import Nlp
 
-#PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.dirname(os.path.abspath(__file__))
 #
 # Importing dataset from Yelp for scalable and automated solution.
 #
+# Unpacking tar file
 #my_tar = tarfile.open('./yelp_dataset.tar')
 #my_tar.extractall(PATH) # specify which folder to extract to
 #my_tar.close()
-#data = json.load(open("./yelp_academic_dataset_review.json", "r"))
-#reviews = pd.DataFrame.from_dict(data, orient="index")
 
+# Loading reviews dataset
+#reviews = pd.read_json("./yelp_academic_dataset_review.json", lines=True, chunksize=20)
+#businesses = pd.read_json("./yelp_academic_dataset_business.json", lines=True, chunksize=20)
 
-reviews = pd.read_excel('./SampleData.xlsx')
-reviews.columns = ['review', 'service', 'date', 'analysis']
-print(reviews)
+#matches = []
+#in_dc = sample['state'] == 'DC'
+#is_open = sample['is_open'] == 1
+#is_postal = sample['postal_code'] == '20005'
+#name = 'salter school'
+#count = 0
+#for sample in businesses:
+    #index = sample.index[in_dc & is_open].tolist()
+    #print(sample.loc[index, 'name'])
+    #gotcha = sample.loc[sample.index[sample['state'].str.lower() == 'bc'], :]
+    #print(gotcha['name'])
+    #for row in sample:
+        #print(row)
+    #count += 1
+    #if count == 5: break
 
-
+test_file = './SampleData.xlsx'
+model = Nlp()
+model.predict(test_file)
--- nlp.py
+++ nlp.py
@@ -10,151 +10,146 @@ import re
 import os
 
 PATH = os.path.dirname(os.path.abspath(__file__))
-url = "GoogleNews-vectors-negative300.bin"
+url = "./GoogleNews-vectors-negative300.bin"
 embeddings = gensim.models.KeyedVectors.load_word2vec_format(url, binary=True)
 
-def prepare_vectors(data):
-    # %% [code]
-    # Preparing vectors for classification
-    docs_vectors = pd.DataFrame()
+class Nlp:
 
-    for doc in data['Review']:
-        temp = pd.DataFrame()  # creating a temporary dataframe
+    def __init__(self):
+        self._model = self._train_model()
+
+    def predict(self, file_path):
+        df = pd.read_excel(file_path)
+        df.drop(['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3'], axis=1, inplace=True)
+        df.drop([6, 10], inplace=True)
+        df.columns = ['Review']
+        df['Sentiment'] = [0] * len(df)
+        df.reset_index(inplace=True)
+        data = df.copy()
+        #print(data.head())
+
+        wl = WordNetLemmatizer()
+        pre_text = []
         
-        for word in doc.split(' '): # looping through each word of a single document and spliting through space
+        # Should make new method clean_data to remove redundancy
+        for i in range(0, len(data)):
+            all_stopwords = stopwords.words('english')
+            all_stopwords.remove('not')
+            review = re.sub('[^a-zA-Z]', ' ', data['Review'][i])
+            review = review.lower()
+            review = review.split()
+            review = [wl.lemmatize(word) for word in review if not word in all_stopwords]
+            review = ' '.join(review)
+            pre_text.append(review)
+
+        pre_data=pd.DataFrame(pre_text)
+        print(pre_data.head())
+
+        data.drop(['Review', 'index'],axis=1,inplace=True)
+        data = pd.concat([pre_data,data],axis=1)
+        print(data.head())
+        data.columns = ['Review', 'Sentiment']
+
+        docs_vectors = self._prepare_vectors(data)
+
+        docs_vectors = docs_vectors.dropna()
+        data['Sentiment'] = self._model.predict(docs_vectors)
+        print(data.head())
+        data.to_excel(PATH + '/output.xlsx')
+
+    def _train_model(self):
+        # Training dataset.
+        url = 'Restaurant_Reviews.tsv'
+        data = pd.read_csv(url, sep='\t', header = 0)
+
+        # Columns renamed
+        data.columns=['Review','Sentiment']
+
+        # Text Preprocessing
+        # Removing numbers, special characters, etc
+        # Removal of stopwords
+        # converting words to lowercase
+        # Lemmatization
+        wl = WordNetLemmatizer()
+
+        pre_text = []
+        for i in range(0, len(data)):
+            all_stopwords = stopwords.words('english')
+            all_stopwords.remove('not')
+            review = re.sub('[^a-zA-Z]', ' ', data['Review'][i])
+            review = review.lower()
+            review = review.split()
+            review = [wl.lemmatize(word) for word in review if not word in all_stopwords]
+            review = ' '.join(review)
+            pre_text.append(review)
+
+        pre_data=pd.DataFrame(pre_text)
+        pre_data.head()
+
+        # %% [code]
+        data.drop('Review',axis=1,inplace=True)
+        data = pd.concat([pre_data,data],axis=1)
+        data.columns =['Review','Sentiment']
+        #print(data.head())
+
+        docs_vectors = self._prepare_vectors(data)
+
+        # %% [code]
+        docs_vectors['Sentiment'] = data['Sentiment']
+        docs_vectors = docs_vectors.dropna()
+
+        # %% [code]
+        # Head of Data
+        #print(docs_vectors.head())
+
+        # %% [code]
+        # Train-Test split of Data
+        from sklearn.model_selection import train_test_split
+
+        x_train, x_test, y_train, y_test = train_test_split(docs_vectors.drop('Sentiment', axis=1),docs_vectors['Sentiment'], random_state=1)
+
+        #print(x_test.head())
+
+        # Support Vector Machine
+        from sklearn import svm
+
+        #Create a svm Classifier
+        clf = svm.SVC(kernel='rbf') # Linear Kernel
+
+        #Train the model using the training sets
+        clf.fit(x_train, y_train)
+
+        # Testing accuracy
+        #y_pred=clf.predict(x_test)
+        #print(y_pred)
+
+        #confusion matrix
+        #from sklearn.metrics import confusion_matrix
+        #cm = confusion_matrix(y_pred, y_test)
+        #print('Confusion Matrix: ', cm)
+
+        #from sklearn.metrics import accuracy_score
+        #accuracy = accuracy_score(y_pred, y_test)
+        #print('Accuracy: ',accuracy)
+
+        return clf
+
+    def _prepare_vectors(self, data):
+        # Preparing vectors for classification
+        docs_vectors = pd.DataFrame()
+
+        for doc in data['Review']:
+            temp = pd.DataFrame()  # creating a temporary dataframe
             
-            try:
-                word_vec = embeddings[word] # if word is present in embeddings(provides weights associate with words(300)) then proceed
-                temp = temp.append(pd.Series(word_vec), ignore_index = True) # if word is present then append it to temporary dataframe
-            except:
-                pass
-        
-        doc_vector = temp.mean() # take the average of each column(w0, w1, w2,........w300)
-        #print(len(doc_vector))
-        docs_vectors = docs_vectors.append(doc_vector, ignore_index = True) # append each document value to the final dataframe
-    return docs_vectors
-
-def predict_sample(model):
-    df = pd.read_excel('./SampleData.xlsx')
-    df.drop(['Unnamed: 1', 'Unnamed: 2', 'Unnamed: 3'], axis=1, inplace=True)
-    df.drop([6, 10], inplace=True)
-    df.columns = ['Review']
-    df['Sentiment'] = [0] * len(df)
-    df.reset_index(inplace=True)
-    data = df.copy()
-    #print(data.head())
-
-    wl = WordNetLemmatizer()
-    pre_text = []
-    
-    # Should make new method clean_data to remove redundancy
-    for i in range(0, len(data)):
-        all_stopwords = stopwords.words('english')
-        all_stopwords.remove('not')
-        review = re.sub('[^a-zA-Z]', ' ', data['Review'][i])
-        review = review.lower()
-        review = review.split()
-        review = [wl.lemmatize(word) for word in review if not word in all_stopwords]
-        review = ' '.join(review)
-        pre_text.append(review)
-
-    pre_data=pd.DataFrame(pre_text)
-    print(pre_data.head())
-
-    data.drop('Review',axis=1,inplace=True)
-    data = pd.concat([pre_data,data],axis=1)
-    data.columns = ['Review', 'Sentiment']
-
-    docs_vectors = prepare_vectors(data)
-
-    docs_vectors = docs_vectors.dropna()
-    df['Sentiment'] = model.predict(docs_vectors)
-    data.to_excel(PATH + '/output.xlsx')
-
-def main():
-    url = 'Restaurant_Reviews.tsv'
-    data = pd.read_csv(url, sep='\t', header = 0)
-
-    # %% [code]
-    # Shape of data
-
-    #print('Rows: ',data.shape[0])
-    #print('Columns: ',data.shape[1])
-
-    # %% [code]
-    # Columns renamed
-
-    data.columns=['Review','Sentiment']
-
-    # %% [code]
-    # Text Preprocessing
-    # Removing numbers, special characters, etc
-    # Removal of stopwords
-    # converting words to lowercase
-    # Lemmatization
-
-    wl = WordNetLemmatizer()
-
-    pre_text = []
-    for i in range(0, len(data)):
-        all_stopwords = stopwords.words('english')
-        all_stopwords.remove('not')
-        review = re.sub('[^a-zA-Z]', ' ', data['Review'][i])
-        review = review.lower()
-        review = review.split()
-        review = [wl.lemmatize(word) for word in review if not word in all_stopwords]
-        review = ' '.join(review)
-        pre_text.append(review)
-
-    pre_data=pd.DataFrame(pre_text)
-    pre_data.head()
-
-    # %% [code]
-    data.drop('Review',axis=1,inplace=True)
-    data = pd.concat([pre_data,data],axis=1)
-    data.columns =['Review','Sentiment']
-    #print(data.head())
-
-    docs_vectors = prepare_vectors(data)
-
-    # %% [code]
-    docs_vectors['Sentiment'] = data['Sentiment']
-    docs_vectors = docs_vectors.dropna()
-
-    # %% [code]
-    # Head of Data
-    #print(docs_vectors.head())
-
-    # %% [code]
-    # Train-Test split of Data
-    from sklearn.model_selection import train_test_split
-
-    x_train, x_test, y_train, y_test = train_test_split(docs_vectors.drop('Sentiment', axis=1),docs_vectors['Sentiment'], random_state=1)
-
-    #print(x_test.head())
-
-    # Support Vector Machine
-    from sklearn import svm
-
-    #Create a svm Classifier
-    clf = svm.SVC(kernel='rbf') # Linear Kernel
-
-    #Train the model using the training sets
-    clf.fit(x_train, y_train)
-
-    y_pred=clf.predict(x_test)
-    #print(y_pred)
-
-    #confusion matrix
-    from sklearn.metrics import confusion_matrix
-    cm = confusion_matrix(y_pred, y_test)
-    #print('Confusion Matrix: ', cm)
-
-    from sklearn.metrics import accuracy_score
-    accuracy = accuracy_score(y_pred, y_test)
-    #print('Accuracy: ',accuracy)
-
-    predict_sample(clf)
-
-if __name__ == '__main__':
-    main()
+            for word in doc.split(' '): # looping through each word of a single document and spliting through space
+                
+                try:
+                    word_vec = embeddings[word] # if word is present in embeddings(provides weights associate with words(300)) then proceed
+                    temp = temp.append(pd.Series(word_vec), ignore_index = True) # if word is present then append it to temporary dataframe
+                except:
+                    pass
+            
+            doc_vector = temp.mean() # take the average of each column(w0, w1, w2,........w300)
+            #print(len(doc_vector))
+            docs_vectors = docs_vectors.append(doc_vector, ignore_index = True) # append each document value to the final dataframe
+        return docs_vectors
