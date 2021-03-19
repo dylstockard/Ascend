0afc89b82e0e10e4539fd257b050fcbbd0f3c897
Dylan Stockard <dylstockard@live.com>
2021-03-19 13:12:58 -0700

    Time filtering
    Added code to filter by current week, previous week, current month, and last month.
    For our demo we will probably want to compare last month and all time since all other filters do not have much data

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import pandas as pd
 #from nlp import Nlp
 from reader import Reader
 import report
+from datetime import date
+from datetime import timedelta
 
 #PATH = os.path.dirname(os.path.abspath(__file__))
 #
@@ -27,8 +29,38 @@ businesses = pd.read_json("./yelp_academic_dataset_business.json", lines=True, c
 # Cleaning data
 df = pd.read_csv('./opentable_reviews.csv')
 df.drop('Unnamed: 0', axis=1, inplace=True)
-c = ('review', 'date', 'overall', 'food', 'service', 'ambiance')
-df.columns = c
+df.columns = ('review', 'date', 'overall', 'food', 'service', 'ambiance')
+
+today = date.today()
+# Get current week
+'''
+curr_week = [today.strftime('%Y-%m-%d'),
+        (today - timedelta(days=1)).strftime('%Y-%m-%d'),
+        (today - timedelta(days=2)).strftime('%Y-%m-%d'),
+        (today - timedelta(days=3)).strftime('%Y-%m-%d'),
+        (today - timedelta(days=4)).strftime('%Y-%m-%d'),
+        (today - timedelta(days=5)).strftime('%Y-%m-%d'),
+        (today - timedelta(days=6)).strftime('%Y-%m-%d')]
+is_curr_week = df['date'].isin(curr_week)
+'''
+# Get previous week
+'''
+prev_week = [(today - timedelta(days=7)).strftime('%Y-%m-%d'),
+            (today - timedelta(days=8)).strftime('%Y-%m-%d'),
+            (today - timedelta(days=9)).strftime('%Y-%m-%d'),
+            (today - timedelta(days=10)).strftime('%Y-%m-%d'),
+            (today - timedelta(days=11)).strftime('%Y-%m-%d'),
+            (today - timedelta(days=12)).strftime('%Y-%m-%d'),
+            (today - timedelta(days=13)).strftime('%Y-%m-%d')]
+is_prev_week = df['date'].isin(prev_week)
+'''
+# Get current month dataframe as curr_month
+is_curr_month = list(filter(lambda x: x.split('-')[1] == today.strftime('%m'), df['date']))
+curr_month = df[df['date'].isin(is_curr_month)]
+# Get previous month dataframe as prev_month
+prev_month = (today.replace(day=1) - timedelta(days=1)).strftime('%m')
+is_prev_month = list(filter(lambda x: x.split('-')[1] == prev_month, df['date']))
+prev_month = df[df['date'].isin(is_prev_month)]
 
 '''
 # Cleaning for sample data
@@ -45,8 +77,8 @@ df.drop(['index'], axis=1, inplace=True)
 #df['sentiment'] = sentiment
 
 # Creating categorizer and printing/saving
-r = Reader(df)
+#r = Reader(df)
 #r.get_categories()
-r.analyze()
+#r.analyze()
 
-report.create_report(df)
+#report.create_report(df)
